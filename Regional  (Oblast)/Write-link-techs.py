e060013a4cc0f53ddf7bbfbe12c10f672d80a77f
Harryvb91 <harryvanbrandenburg@hotmail.com>
2022-12-12 16:58:20 +0100

    12-12 update

--- Regional  (Oblast)/Write-link-techs.py
+++ Regional  (Oblast)/Write-link-techs.py
@@ -22,26 +22,25 @@ import json
         
         
 excel= pd.read_excel(r'C:\Users\Harrie\Desktop\Thesis\data\Transmission\Transmission.xlsx')
-df= pd.DataFrame(excel, columns=['FT',"Capacity"])
+df= pd.DataFrame(excel, columns=['FT',"Cap_sum"])
+
+d_links={"Links": {df.at[0,'FT']:{"techs": {"ac_transmission": {"constraints": {"energy_cap" :{}}}}}}}
 
 
-def build_links(n):
-    d_energy_cap = {"energy_cap": int(df.at[n,'Capacity'])}
+def build_dict2(n):
+    # d_links['Links'][df.at[n,'FT']]['techs']['ac_transmission']['constraints']['energy_cap']= df.at[n,'FT']
+    d_energy_cap = {"energy_cap": int(df.at[n,'Cap_sum'])}
     d_constraints= {"constraints": {"energy_cap": d_energy_cap}}
     d_ac_transmission = {"ac_transmission": d_constraints}
     d_techs = {"techs": d_ac_transmission}
-    d_region = {df.at[n,'FT']: d_techs}
-    #d_links = {"Links": d_region}
-    d_links['Links'][df.at[n,'FT']] = d_region
+    # d_region = {df.at[n,'FT']: d_techs}
+    d_links['Links'][df.at[n,'FT']] = d_techs
     return d_links
 
 
-
-
-
-d_links['Links'][df.at[2,'FT']]= d_region
+# d_links['Links'][df.at[2,'FT']]= d_region2
 for n in range(len(df)):
-    d_links = build_links(n)
+    d_links = build_dict2(n)
 
 with open('UA-Link-all-neigbours.yaml', "w") as f:
       yaml.dump(d_links, f)
